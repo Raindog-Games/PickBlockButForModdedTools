969221638bcd5cb0d2801ed278101191da83e311
Benjamin Slater <benjamin.ed.slater@gmail.com>
2022-10-12 15:29:45 -0700

    updated to 1.19

--- src/main/java/ca/raindoggames/quickpickmoddedtool/QuickPickModdedTool.java
+++ src/main/java/ca/raindoggames/quickpickmoddedtool/QuickPickModdedTool.java
@@ -1,7 +1,6 @@
 package ca.raindoggames.quickpickmoddedtool;
 
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -9,8 +8,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.mojang.blaze3d.platform.InputConstants;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("quickpickmoddedtool")
@@ -31,14 +28,5 @@ public class QuickPickModdedTool
     
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-    	// Save keyBindings 
-        keyBindings = new KeyMapping[4];
-        keyBindings[0] = new KeyMapping("key.quickpickmoddedtool.break", InputConstants.KEY_B, "category.quickpickmoddedtool.utils");
-        keyBindings[1] = new KeyMapping("key.quickpickmoddedtool.save", InputConstants.KEY_V, "category.quickpickmoddedtool.utils"); 
-        keyBindings[2] = new KeyMapping("key.quickpickmoddedtool.pickarang", InputConstants.KEY_Y, "category.quickpickmoddedtool.utils");
-        keyBindings[3] = new KeyMapping("key.quickpickmoddedtool.atomicdisassembler", InputConstants.KEY_X, "category.quickpickmoddedtool.utils");
-        for (int i = 0; i < keyBindings.length; i++) {
-        	ClientRegistry.registerKeyBinding(keyBindings[i]);
-        }
     }
 }
---
+++ src/main/java/ca/raindoggames/quickpicktool/controls/KeyRegisterEventHandler.java
@@ -0,0 +1,31 @@
+package ca.raindoggames.quickpicktool.controls;
+
+import static ca.raindoggames.quickpickmoddedtool.QuickPickModdedTool.LOGGER;
+
+import com.mojang.blaze3d.platform.InputConstants;
+
+import ca.raindoggames.quickpickmoddedtool.QuickPickModdedTool;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class KeyRegisterEventHandler {
+	@SubscribeEvent
+	public static void registerKeys(RegisterKeyMappingsEvent e) {
+    	KeyMapping[] keyBindings = QuickPickModdedTool.keyBindings;
+    	LOGGER.info("Registering keys");
+    	// Save keyBindings 
+    	keyBindings = new KeyMapping[2];
+        keyBindings[0] = new KeyMapping("key.quickpickmoddedtool.break", InputConstants.KEY_B, "category.quickpickmoddedtool.utils");
+        keyBindings[1] = new KeyMapping("key.quickpickmoddedtool.save", InputConstants.KEY_V, "category.quickpickmoddedtool.utils"); 
+        keyBindings[2] = new KeyMapping("key.quickpickmoddedtool.pickarang", InputConstants.KEY_Y, "category.quickpickmoddedtool.utils");
+        keyBindings[2] = new KeyMapping("key.quickpickmoddedtool.atomicdisassembler", InputConstants.KEY_X, "category.quickpickmoddedtool.utils");
+    	for (int i = 0; i < keyBindings.length; i++) {
+    		e.register(keyBindings[i]);
+    	}
+    	QuickPickModdedTool.keyBindings = keyBindings;
+  	}
+}
