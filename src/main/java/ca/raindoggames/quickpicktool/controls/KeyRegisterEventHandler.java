46f4b17cfc6da1d415a046e6829a86f89d804d3d
Benjamin Slater <benjamin.ed.slater@gmail.com>
2022-10-12 15:55:12 -0700

    corrected key mapping

--- src/main/java/ca/raindoggames/quickpicktool/controls/KeyRegisterEventHandler.java
+++ src/main/java/ca/raindoggames/quickpicktool/controls/KeyRegisterEventHandler.java
@@ -18,11 +18,11 @@ public class KeyRegisterEventHandler {
     	KeyMapping[] keyBindings = QuickPickModdedTool.keyBindings;
     	LOGGER.info("Registering keys");
     	// Save keyBindings 
-    	keyBindings = new KeyMapping[2];
+    	keyBindings = new KeyMapping[4];
         keyBindings[0] = new KeyMapping("key.quickpickmoddedtool.break", InputConstants.KEY_B, "category.quickpickmoddedtool.utils");
         keyBindings[1] = new KeyMapping("key.quickpickmoddedtool.save", InputConstants.KEY_V, "category.quickpickmoddedtool.utils"); 
         keyBindings[2] = new KeyMapping("key.quickpickmoddedtool.pickarang", InputConstants.KEY_Y, "category.quickpickmoddedtool.utils");
-        keyBindings[2] = new KeyMapping("key.quickpickmoddedtool.atomicdisassembler", InputConstants.KEY_X, "category.quickpickmoddedtool.utils");
+        keyBindings[3] = new KeyMapping("key.quickpickmoddedtool.atomicdisassembler", InputConstants.KEY_X, "category.quickpickmoddedtool.utils");
     	for (int i = 0; i < keyBindings.length; i++) {
     		e.register(keyBindings[i]);
     	}
