be3ffe743bc23baa998836f59f5230c8447ac866
Benjamin Slater <benjamin.ed.slater@gmail.com>
2023-07-09 18:05:02 -0700

    corrected registry update on tags

--- src/main/java/ca/raindoggames/quickpickmoddedtool/blocktags/ToolBlockTags.java
+++ src/main/java/ca/raindoggames/quickpickmoddedtool/blocktags/ToolBlockTags.java
@@ -1,11 +1,11 @@
 package ca.raindoggames.quickpickmoddedtool.blocktags;
 
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class ToolBlockTags {
-	public static final TagKey<Block> SILK_TOUCH_MINEABLE = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("quickpicktool", "mineable/silk_touch"));
-	public static final TagKey<Block> SHEAR_MINEABLE = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("quickpicktool", "mineable/shear"));
+	public static final TagKey<Block> SILK_TOUCH_MINEABLE = TagKey.create(Registries.BLOCK, new ResourceLocation("quickpicktool", "mineable/silk_touch"));
+	public static final TagKey<Block> SHEAR_MINEABLE = TagKey.create(Registries.BLOCK, new ResourceLocation("quickpicktool", "mineable/shear"));
 }
